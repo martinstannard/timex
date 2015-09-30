3f186db8d8ee1edc5336530d9986796ce6d651f5
Martin Stannard <martin@stannard.id.au>
2015-09-30 14:36:14 +1000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Timex.Mixfile do
   def project do
     [ app: :timex,
       version: "0.19.5",
-      elixir: "~> 1.0",
+      elixir: "~> 1.1",
       description: "A date/time library for Elixir",
       package: package,
       deps: deps ]
